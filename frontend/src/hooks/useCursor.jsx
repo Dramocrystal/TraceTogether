7d1eefbeb7144f3211a1f92ae725f204b11de91c
Dramocrystal <asselahyazid@gmail.com>
2025-01-05 22:43:47 -0500

    zoom/dezoom + move around canvas

--- frontend/src/hooks/useCursor.jsx
+++ frontend/src/hooks/useCursor.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const useCursor = ({ overlayCanvasRef }) => {
+export const useCursor = ({ overlayCanvasRef, zoom }) => {
   const [cursorPositions, setCursorPositions] = useState({});
 
   const renderCursors = () => {
@@ -12,18 +12,21 @@ export const useCursor = ({ overlayCanvasRef }) => {
     
     Object.entries(cursorPositions).forEach(([user, position]) => {
       const { x, y } = position;
+      const cursorSize = Math.max(8 / zoom, 8);
+      const fontSize = Math.max(16 / zoom, 16);
       context.fillStyle = 'red';
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2);
+      context.arc(x, y, cursorSize, 0, Math.PI * 2);
       context.fill();
       context.fillStyle = 'black';
-      context.fillText(user, x + 10, y);
+      context.font = `${fontSize}px Arial`;
+      context.fillText(user, x + cursorSize + 5, y + cursorSize / 2);
     });
   };
 
   useEffect(() => {
     renderCursors();
-  }, [cursorPositions]);
+  }, [cursorPositions, zoom]);
 
   return { 
     cursorPositions,
