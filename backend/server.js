84ce4ad72f354aa8da53b35dd478ec305c3f747a
Yazid Asselah <69166231+Dramocrystal@users.noreply.github.com>
2025-05-08 01:07:44 -0400

    Update server.js
    changed to port 80

--- backend/server.js
+++ backend/server.js
@@ -260,7 +260,7 @@ function printRooms() {
 
 
 // Start server
-const PORT = 5000;
+const PORT = 80;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
